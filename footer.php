7a6014dcd2d358ad293dc4a0bf4e2e2b7515abde
Trevor Davis <davist11@gmail.com>
2011-07-11 23:41:53 -0400

    add GA back in

--- footer.php
+++ footer.php
@@ -19,5 +19,15 @@
 		<script src="http://platform.twitter.com/widgets.js"></script>
 	<?php endif; ?>
 	<script src="http://connect.facebook.net/en_US/all.js#xfbml=1"></script>
+	
+	<script type="text/javascript">
+	var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+	document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+	</script>
+	<script type="text/javascript">
+	try {
+	var pageTracker = _gat._getTracker("UA-11239005-1");
+	pageTracker._trackPageview();
+	} catch(err) {}</script>
 </body>
 </html>
